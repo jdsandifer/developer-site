c991f0b6832e49946375cb51d88e1c29d4f08dd2
J.D. Sandifer <sandifer.jd@gmail.com>
2018-09-25 11:17:09 -0700

    Fix requireds in contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -208,9 +208,9 @@ class HomeIndex extends React.Component {
                             <div className="8u 12u$(small)">
                                 <form method="post" action="mailto:sandifer.jd@gmail.com" encType="text/plain">
                                     <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" required/></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" required/></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4" required></textarea></div>
+                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" required={true}/></div>
+                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" required={true}/></div>
+                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4" required={true}></textarea></div>
                                     </div>
                                 </form>
                                 <ul className="actions">
