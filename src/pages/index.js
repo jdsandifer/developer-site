dd7d9bae57d2872bb6f85a44b6b39b92c85a1ca9
J.D. Sandifer <sandifer.jd@gmail.com>
2019-01-24 23:05:37 -0800

    Fix typo, change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,14 +142,14 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>JavaScript, React, and PHP Developer</h2>
+                            <h2>Software Developer</h2>
                         </header>
                         <p>
                             I love to remove obstacles between people and technology. Whether it's improving the user interface, optimizing the back-end, 
                             or tweaking database calls - wherever problems exist, I want to dive in and solve them.
                         </p>
                         <p>
-                            I also love collaboratiing with other people who are passionate about what they do. Teammates usually 
+                            I also love collaborating with other people who are passionate about what they do. Teammates usually 
                             describe me as calm, casual, and easy-going.
                         </p>
                         {/*
