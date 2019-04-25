c8ea0eee5e5a97c50f6b941f0b6f7bdde4ce1980
J.D. Sandifer <sandifer.jd@gmail.com>
2019-04-25 10:14:22 -0700

    Update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ class Footer extends React.Component {
                             </a>
                         </li>
                         <li>
-                            <a  href="https://www.linkedin.com/in/jdsandifer/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                            <a href="https://www.linkedin.com/in/jdsandifer/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
                         <li>
                             <a href="mailto:sandifer.jd@gmail.com" className="icon fa-envelope-o">
                                 <span className="label">Email</span>
@@ -25,15 +25,15 @@ class Footer extends React.Component {
                         </li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; 2018 J.D. Sandifer</li>
-                        <li>Design:&nbsp; 
+                        <li>&copy; 2018 & 2019 J.D. Sandifer</li>
+                        <li>Design:&nbsp;
                             <a href="http://html5up.net">
-                                 HTML5 UP 
+                                HTML5 UP
                             </a>
                         </li>
-                        <li>Photos:&nbsp;  
+                        <li>Photos:&nbsp;
                             <a href="https://www.pexels.com/">
-                            PEXELS 
+                                PEXELS
                             </a>
                         </li>
                     </ul>
